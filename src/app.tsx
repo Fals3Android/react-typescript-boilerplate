5483bb24bbe8168058d4ab980575c338f27cf673
Fals3Andro1d <davilasturias89@gmail.com>
2018-07-14 13:21:52 -0700

    Initial Commit

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+console.log('test');
+ReactDOM.render(
+    <div>React Typescript Boilerplate</div>,
+    document.getElementById('root')
+);
